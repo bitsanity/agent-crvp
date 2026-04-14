b4a41ee76818b61f93a2ae3c3f493c078717fdb2
bitsanity <brydiver@gmail.com>
2026-04-14 14:41:25 -0700

    .

---
+++ cgi-bin/requests.js
@@ -0,0 +1,109 @@
+
+// NOTE:
+//   - reqests database is a fileset with one file per client
+//   - file contains an array of objects, each object is a request-result
+
+const fs = require( 'node:fs' )
+
+function isString(val) {
+  return Object.prototype.toString.call(val) === '[object String]';
+}
+
+function toFilename( pubkeyhex ) {
+  return './requests/' + pubkeyhex
+}
+
+// returns JSON array
+function readFile( pubkeyhex ) {
+  try {
+    let fname = toFilename( pubkeyhex )
+    return JSON.parse( fs.readFileSync(fname, 'utf-8') )
+  }
+  catch (e) { }
+  return null
+}
+
+function writeFile( pubkeyhex, dataobj ) {
+  let fname = toFilename( pubkeyhex )
+  fs.writeFileSync( fname, JSON.stringify(data), { encoding:'utf-8' } )
+}
+
+function newFile( pubkeyhex ) {
+  writeFile( pubkeyhex, [] )
+}
+
+function append( pubkeyhex, anobj ) {
+  let reqarr = readFile( pubkeyhex )
+  reqarr.push( anobj )
+  writeFile( pubkeyhex, reqarr )
+}
+
+function replace( pubkeyhex, cookie, obj ) {
+  let records = exports.all( pubkeyhex )
+  if (!records || records.length == 0) throw 'record not found'
+
+  let cookiestr = isString(cookie) ? cookie : JSON.stringify( cookie )
+
+  for( let ii = 0; ii < records.length; ii++ ) {
+    let rec = records[ii]
+    if (    rec.redreq && rec.redreq.cookie
+         && rec.redreq.cookie === cookiestr ) {
+      records[ii] = obj
+      return
+    }
+  }
+
+  throw 'record not found'
+}
+
+exports.get = function( pubkeyhex, cookie ) {
+  let records = readFile( pubkeyhex )
+  if (!records || records.length == 0) return null
+
+  let cookiestr = isString(cookie)
+                  ? cookie
+                  : JSON.stringify( cookie )
+
+  for( let ii = 0; ii < records.length; ii++ ) {
+    let rec = records[ii]
+
+    if (    rec.redreq
+         && rec.redreq.cookie
+         && rec.redreq.cookie === cookiestr ) {
+      return rec
+    }
+  }
+
+  return null
+}
+
+exports.all = function( pubkeyhex ) {
+  return readFile( pubkeyhex )
+}
+
+
+exports.add = function( pubkeyhex, reqobj ) {
+  if (!readFile(pubkeyhex)) {
+    newFile( pubkeyhex )
+  }
+
+  let data = {
+    created: Date.now(),
+    redreq: reqobj,
+    redres: null,
+    completed: null
+  }
+
+  append( pubkeyhex, data )
+}
+
+exports.setResult = function( pubkeyhex, cookie, resultobj ) {
+  let rec = exports.get( pubkeyhex, cookie )
+  if (!rec) throw 'request not found'
+
+  rec.redres = resultobj
+  rec.completed = Date.now()
+
+  replace( pubkeyhex, cookie, rec )
+}
+
---
+++ cgi-bin/worker.js
@@ -0,0 +1,128 @@
+#!node
+
+const session = require( './sessions.js' )
+const ecjson = require( './ecjsonrpc.js' )
+const requests = require( './requests.js' )
+const acl = require( './acl.js' )
+
+function screen( pubkeyhex ) {
+
+  let caller = acl.getRecord( pubkeyhex )
+  if (!caller) {
+    console.log( "Status: 401 Unauthorized" )
+    console.log( "Content-Type: text/plain\r\n\r\n" )
+    console.log( paramobj.spkhex + " is not on our ACL. Please register." )
+    System.exit( 0 )
+  }
+  else {
+    if (caller.bannedDate) {
+      console.log( "Status: 403 Forbidden" )
+      console.log( "Content-Type: text/plain\r\n\r\n" )
+      console.log( paramobj.spkhex + " recognized, but banned due to: " +
+                   caller.bannedReason )
+    }
+    System.exit( 0 )
+  }
+}
+
+function toRedObj( blkobj ) {
+  let privkeyhex = process.env.AGENT_PRIVKEYHEX
+
+  if (!privkeyhex) {
+    console.log( "Status: 503 Service Unavailable" )
+    console.log( "Content-Type: text/plain\r\n\r\n" )
+    console.log( "maintenance required" )
+    System.exit( 0 )
+  }
+
+  let redobj = ecjson.blackToRed( privkeyhex, blkobj )
+  if (!redobj) {
+    console.log( "Status: 400 Bad Request" )
+    console.log( "Content-Type: text/plain\r\n\r\n" )
+    console.log( "signature validation/decryption failed" )
+    System.exit( 0 )
+  }
+
+  return redobj
+}
+
+function doAny( paramobj ) {
+
+  let redobj
+
+  if (paramobj.msghex) {
+    screen( paramobj.spkhex )
+    redobj = toRedObj( paramobj )
+  }
+  else {
+    // anonymous unencrypted/red request object - do we just trust it?
+    // TODO
+
+    redobj = paramobj
+  }
+
+  // everything ok if we got this far add to request queue
+  requests.add( paramobj.spkhex, redobj )
+
+  console.log( "Status: 200 OK" )
+  console.log( "Content-Type: text/plain" )
+  console.log( "ACK" )
+  System.exit( 0 )
+}
+
+function doGet() {
+
+  let reqobj = {}
+  let args = process.env.QUERY_STRING.split('&')
+
+  for ( var ii = 0; ii < args.length; ii++ ) {
+    let arg = args[ii].split('=')
+    let argname = arg[0], argval = arg[1]
+    reqobj[ argname ] = argval
+  }
+
+  doAny( reqobj ) 
+}
+
+function doPost() {
+
+  process.stdin.on('data', data => {
+    let body = JSON.parse( data.toString() )
+    doAny( body )
+  });
+
+}
+
+// handle receiving a request from another agent
+
+module.exports.dorequest = function() {
+
+  try {
+    if (process.env.REQUEST_METHOD === 'GET') {
+      doGet()
+    }
+    else if (process.env.REQUEST_METHOD === 'POST') {
+      doPost()
+    }
+    else
+      throw "unrecognized REQUEST_METHOD: " + process.env.REQUEST_METHOD
+  }
+  catch( ex ) {
+    console.log( "Status: 400 Bad Request" )
+    console.log( "Content-Type: text/plain\r\n\r\n" )
+    console.log( ex.toString() )
+  }
+
+  System.exit( 0 )
+}
+
+// handle receiving the result of some request we sent to another agents
+// web interface
+
+module.exports.doresult = function() {
+
+  // forward the result to our agent
+  // TODO
+
+}
+
