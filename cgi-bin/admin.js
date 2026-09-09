668d64a9ddff2f2a1456ceb886ae4582c94dfcac
bitsanity <brydiver@gmail.com>
2026-09-09 13:46:05 -0700

    .

--- cgi-bin/admin.js
+++ cgi-bin/admin.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/node --jitless
 
 const HTTPRESPONSES = {
   102 : "Processing",
--- cgi-bin/env.js
+++ cgi-bin/env.js
@@ -1,10 +1,11 @@
 exports.VARS = {
-  ETHERSCAN_API_KEY: "etherscan.io api key",
-  AGENT_ETH_ADDRESS: "agents ethereum address 0x...",
-  AGENT_PRIVKEYHEX: "private key in hex, no 0x at the start",
-  AGENT_PUBKEY: "pubkey in hex, prefer compressed, no 0x preamble",
+  ETHERSCAN_API_KEY: "",
+
+  // Nautilus agent identity
+  AGENT_ETH_ADDRESS: "0x399283116FdBe8963b77999997C2DBd9f452F4A2",
+  AGENT_PRIVKEYHEX: "5e28082f6f8b56eaafd135bd88ea695d3815748d4d88f564c22695ecf064cb39",
+  AGENT_PUBKEY: "0308e165a74f269feb8a2e7f3397718925febbebabd0d98cddbe52b3fd67a15257",
 
   // note: ESCROBOT_SCA is public, smart contract is free for anyone to use
   ESCROBOT_SCA: "0xe9a7520fFc67b808bec949496eB97e96bf3e0C70"
 }
-
