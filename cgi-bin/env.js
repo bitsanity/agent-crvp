c03fb96a15a18cf41acc7df7633a7b4adb8083e3
Bryan <15133466+bitsanity@users.noreply.github.com>
2026-09-13 12:22:50 -0700

    Clear agent identity variables in env.
    Updated agent identity variables to empty strings for security.

--- cgi-bin/env.js
+++ cgi-bin/env.js
@@ -1,10 +1,10 @@
 exports.VARS = {
   ETHERSCAN_API_KEY: "",
 
-  // Nautilus agent identity
-  AGENT_ETH_ADDRESS: "0x399283116FdBe8963b77999997C2DBd9f452F4A2",
-  AGENT_PRIVKEYHEX: "5e28082f6f8b56eaafd135bd88ea695d3815748d4d88f564c22695ecf064cb39",
-  AGENT_PUBKEY: "0308e165a74f269feb8a2e7f3397718925febbebabd0d98cddbe52b3fd67a15257",
+  // agent identity
+  AGENT_ETH_ADDRESS: "",
+  AGENT_PRIVKEYHEX: "",
+  AGENT_PUBKEY: "",
 
   // note: ESCROBOT_SCA is public, smart contract is free for anyone to use
   ESCROBOT_SCA: "0xe9a7520fFc67b808bec949496eB97e96bf3e0C70"
